c2f540509df6b683ffecd79ab976c7b1730f9e61
Vladimir Kutuev <vladimir.kutuev@gmail.com>
2026-01-18 20:49:14 +0300

    Add arguments parsing to main script

--- main.py
+++ main.py
@@ -2,24 +2,28 @@ import argparse
 from pathlib import Path
 
 from translator.languages import Languages
-from translator.llm import LlmTranslator
+from translator.translator import LlmTranslator
 from translator.pipeline import TranslatorPipeline
 from translator.studio.factory import TrikStudioFactory
 
 
 def main():
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("path", type=str, help="Path to directory with trik studio translations")
-    # parser.add_argument("--with-translation", action="store_true", help="Whether add translation to output JSON")
-    # args = parser.parse_args()
-    # path = Path(args.path)
-    studio_dir = Path("../trik-studio/")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path", type=str,
+                        help="Path to directory with your program, for example, the path to TRIKStudio.")
+    parser.add_argument("lang_tr", type=str, choices=list(Languages),
+                        help="The language into which the phrases in your program need to be translated.")
+    parser.add_argument("--lang_ex", type=str, choices=list(Languages), required=False,
+                        help="""The language that translations should be based on as an example.
+                        This may be useful when using an LLM-based translator.""")
+    args = parser.parse_args()
+
     pipeline = TranslatorPipeline(
         TrikStudioFactory(),
-        studio_dir,
+        Path(args.path),
         LlmTranslator(),
     )
-    pipeline.run(Languages.RU)
+    pipeline.run(args.lang_tr, args.lang_ex)
 
 
 if __name__ == "__main__":
